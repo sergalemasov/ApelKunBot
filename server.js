5328508bc657a64b75af5db10429565021538a57
Sergei Alemasov <Sergei_Alemasov@epam.com>
2017-10-02 19:42:00 +0400

    moved app id and password to azure

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var MICROSOFT_APP_ID = '38d7eda8-a3a8-4cc4-96aa-2c6392ff0b17';
-var MICROSOFT_APP_PASSWORD = 'r8ugUzyLjn5tuN3FzziGbbY';
-
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -12,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: MICROSOFT_APP_ID,
-    appPassword: MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
