3048efd378b1f7ee6c76e341eea311fbc8407a13
Roman_Pechersky <roman_pechersky@epam.com>
2017-10-05 18:37:42 +0400

    extended random function

--- apis/random.js
+++ apis/random.js
@@ -1,16 +1,43 @@
 function random(session) {
-  var names = [
-    'Стас',
-    'Влад',
-    'Вадим',
-    'Сережа'
+  var mentors = [
+    'Stas Osipov',
+    'Vlad Mironov',
+    'Vadim Gorsky',
+    'Sergey Alemasov'
   ];
 
-  var match = /^(?:@ApelKunBot\s)?(who\?)?$/.exec(session.message.text);
+  var allDevs = [
+    'Stas Osipov',
+    'Vlad Mironov',
+    'Vadim Gorsky',
+    'Sergey Alemasov',
+    'Vlad Danilov',
+    'Viktor Vavilov',
+    'Ramil Baltachev',
+    'Ilias Musin',
+    'Pavel Kazakov'
+  ];
 
-  if (match) {
-    session.send("Итак, доклад проведет для нас: " + names[Math.floor(Math.random() * names.length)]);
+  var mentees = [
+    'Vlad Danilov',
+    'Viktor Vavilov',
+    'Ramil Baltachev',
+    'Ilias Musin',
+    'Pavel Kazakov'
+  ];
+
+  var matchLeads = /^(?:@ApelKunBot\s)?(who\? leads)?$/.exec(session.message.text);
+  var matchDevs = /^(?:@ApelKunBot\s)?(who\? devs)?$/.exec(session.message.text);
+  var matchAll = /^(?:@ApelKunBot\s)?(who\? all)?$/.exec(session.message.text);
+
+  if (matchLeads) {
+    session.send('Bot has chosen ' + mentors[Math.floor(Math.random() * mentors.length)] + '. You now you should fulfill Roman\'s desire');
+  } else if (matchDevs) {
+    session.send('Bot has chosen ' + allDevs[Math.floor(Math.random() * allDevs.length)] + '. You now you should fulfill Roman\'s desire');
+  } else if (matchAll) {
+  session.send('Bot has chosen ' + mentees[Math.floor(Math.random() * mentees.length)] + '. You now you should fulfill Roman\'s desire');
   }
+
 
 }
 
