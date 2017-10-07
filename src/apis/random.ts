c66dd204ca1dd60050237f6c94c9f78f83e9b3f6
Roman_Pechersky <roman_pechersky@epam.com>
2017-10-07 17:13:47 +0400

    fixed inconsistency

--- src/apis/random.ts
+++ src/apis/random.ts
@@ -26,18 +26,17 @@ function random(session) {
     'Pavel Kazakov'
   ];
 
-  var matchLeads = /^(?:@ApelKunBot\s)?(who\? leads)?$/.exec(session.message.text);
-  var matchDevs = /^(?:@ApelKunBot\s)?(who\? devs)?$/.exec(session.message.text);
-  var matchAll = /^(?:@ApelKunBot\s)?(who\? all)?$/.exec(session.message.text);
+  var matchMentors = /^(?:@ApelKunBot\s)?(who\? leads)?$/.exec(session.message.text);
+  var matchMentees = /^(?:@ApelKunBot\s)?(who\? devs)?$/.exec(session.message.text);
+  var matchAllDevs = /^(?:@ApelKunBot\s)?(who\? all)?$/.exec(session.message.text);
 
-  if (matchLeads) {
+  if (matchMentors) {
     session.send('Bot has chosen ' + mentors[Math.floor(Math.random() * mentors.length)] + '. You now you should fulfill Roman\'s desire');
-  } else if (matchDevs) {
-    session.send('Bot has chosen ' + allDevs[Math.floor(Math.random() * allDevs.length)] + '. You now you should fulfill Roman\'s desire');
+  } else if (matchMentees) {
+    session.send('Bot has chosen ' + mentees[Math.floor(Math.random() * mentees.length)] + '. You now you should fulfill Roman\'s desire');
   } else if (matchAll) {
-  session.send('Bot has chosen ' + mentees[Math.floor(Math.random() * mentees.length)] + '. You now you should fulfill Roman\'s desire');
+  session.send('Bot has chosen ' + allDevs[Math.floor(Math.random() * allDevs.length)] + '. You now you should fulfill Roman\'s desire');
   }
-
 
 }
 
