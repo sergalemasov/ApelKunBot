17fd37ccb030606ac32b3eea5d6a15b31279678f
Apel-kun <apel.kun@gmail.com>
2017-10-09 21:43:16 +0400

    added da ved file

---
+++ src/apis/da-ved.ts
@@ -0,0 +1,16 @@
+import { Session } from 'botbuilder';
+import MessageHandler from '../utils/message-handler';
+
+export default class DaVed extends MessageHandler {
+  protected handleBotMessage(session: Session): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const match = /^да\sведь\?$/.exec(session.message.text);
+      if (!match) {
+        resolve(null);
+        return;
+      }
+
+      resolve('да, дружище, я абсолютно с тобой согласен!');
+    });
+  }
+}
