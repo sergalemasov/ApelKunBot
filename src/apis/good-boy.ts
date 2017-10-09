4ddbe24156741a812d05245d75fb1f892754acac
Apel-kun <apel.kun@gmail.com>
2017-10-09 22:53:51 +0400

    added new boys

--- src/apis/good-boy.ts
+++ src/apis/good-boy.ts
@@ -2,7 +2,15 @@ import fs = require('fs');
 import path = require('path');
 import { Session, Message } from 'botbuilder';
 import MessageHandler from '../utils/message-handler';
+import _ = require('lodash');
 
+const goodBoys = [
+  'good_boy.jpg',
+  'good_boy1.jpg',
+  'good_boy2.jpg',
+  'good_boy3.jpg',
+  'good_boy4.jpg',
+];
 export default class GoodBoy extends MessageHandler {
   protected handleBotMessage(session: Session): Promise<string|Message> {
     return new Promise((resolve, reject) => {
@@ -18,12 +26,12 @@ export default class GoodBoy extends MessageHandler {
 
   private readGoodBoy(session: Session): Promise<string|Message> {
     return new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, '../assets/img/good_boy.png'), function(err, data) {
+      fs.readFile(path.join(__dirname, `../assets/img/${_.sample(goodBoys)}`), function(err, data) {
         if (err) {
           resolve('I am not a good boy!');
           return;
         }
-        const contentType = 'image/png';
+        const contentType = 'image/jpeg';
         const base64 = Buffer.from(data).toString('base64');
 
         resolve(
