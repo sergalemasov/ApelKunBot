64d876a1f7bf1637d58dbe58d57bf3b25c11fa65
Apel-kun <apel.kun@gmail.com>
2017-10-15 00:47:12 +0400

    http action test some fixes

--- src/apis/http-action-test.ts
+++ src/apis/http-action-test.ts
@@ -1,4 +1,10 @@
-import { Session, Message } from 'botbuilder';
+import {
+  Session,
+  Message,
+  HeroCard,
+  CardAction,
+  CardImage
+} from 'botbuilder';
 import MessageHandler from '../utils/message-handler';
 
 export default class HttpActionTest extends MessageHandler {
@@ -16,73 +22,15 @@ export default class HttpActionTest extends MessageHandler {
 
   private respond(session: Session): Message {
     var msg = new Message(session)
-    .addAttachment({
-      contentType: "application/vnd.microsoft.card.adaptive",
-      content: {
-        type: "AdaptiveCard",
-        speak: "<s>Your  meeting about \"Adaptive Card design session\"<break strength='weak'/> is starting at 12:30pm</s><s>Do you want to snooze <break strength='weak'/> or do you want to send a late notification to the attendees?</s>",
-        body: [
-            {
-              "type": "TextBlock",
-              "text": "Adaptive Card design session",
-              "size": "large",
-              "weight": "bolder"
-            },
-            {
-              "type": "TextBlock",
-              "text": "Conf Room 112/3377 (10)"
-            },
-            {
-              "type": "TextBlock",
-              "text": "12:30 PM - 1:30 PM"
-            },
-            {
-              "type": "TextBlock",
-              "text": "Snooze for"
-            },
-            {
-              "type": "Input.ChoiceSet",
-              "id": "snooze",
-              "style":"compact",
-              "choices": [
-                {
-                  "title": "5 minutes",
-                  "value": "5",
-                  "isSelected": true
-                },
-                {
-                  "title": "15 minutes",
-                  "value": "15"
-                },
-                {
-                  "title": "30 minutes",
-                  "value": "30"
-                }
-              ]
-            }
-        ],
-        "actions": [
-          {
-            "type": "Action.Http",
-            "method": "POST",
-            "url": "http://foo.com",
-            "title": "Snooze"
-          },
-          {
-            "type": "Action.Http",
-            "method": "POST",
-            "url": "http://foo.com",
-            "title": "I'll be late"
-          },
-          {
-            "type": "Action.Http",
-            "method": "POST",
-            "url": "http://foo.com",
-            "title": "Dismiss"
-          }
-        ]
-      }
-    });
+      .attachments([
+        new HeroCard(session)
+          .title("Classic Gray T-Shirt")
+          .subtitle("100% Soft and Luxurious Cotton")
+          .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+          .buttons([
+            CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+          ])
+      ]);
     return msg;
   }
 }
